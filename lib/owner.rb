f84d06a1274366615fde582e77f4e1623738e2c4
Logan Smith <logsmith06@gmail.com>
2019-11-27 01:20:03 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,11 @@
 class Owner
   # code goes here
   attr_reader :name, :species
+  @@all = []
   def initialize(name, species = "human")
     @name = name
     @species = species
+    @@all << self
   end
 
   def say_species
@@ -11,6 +13,6 @@ class Owner
   end
 
   def self.all
-    self.all
+    @@all
   end
 end
